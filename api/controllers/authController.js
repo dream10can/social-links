6708f0d18ab3338701c13ecbf40b930d3ad720be
dream10can <dream10candeveloper@gmail.com>
2024-12-07 20:11:56 +0300

    change secure in cookie

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -109,7 +109,7 @@ const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        // secure:true,
+        secure: true,
         maxAge: expireDate,
       })
       .status(200)
