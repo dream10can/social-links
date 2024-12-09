ffa4eec843b048292eab39eec34f7e36e6c96b9b
dream10can <dream10candeveloper@gmail.com>
2024-12-09 11:19:25 +0300

    change same site option

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -110,7 +110,7 @@ const login = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "none",
+        // sameSite: "none",
         maxAge: expireDate,
       })
       .status(200)
